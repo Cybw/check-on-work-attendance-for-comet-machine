4d46e5370163fd70f474ee8ab1ebfb8bd4a90f19
Cybw <wangshuaibo@bupt.edu.cn>
2019-03-08 15:35:34 +0800

    Update kaoqin.py

--- release/kaoqin.py
+++ release/kaoqin.py
@@ -9,7 +9,7 @@ Created on Mon Mar  4 10:30:40 2019
 使用说明：
     1、“coursetab.csv”文件为教务处公布的每日上课时间段；
     2、“coursetime.csv”文件为参与打卡的每位同学的上课时间（所有
-    参与打卡的同学必须都包含在表内，学校助管、公共活动等都按上课处理）
+    参与打卡的同学必须都包含在表内，学校助管、公共活动等都可按上课处理）
 	表中从周一到周五，‘o’为不上课，‘1’为上课；
     3、使用前请将考勤报表上传至脚本同一目录下；
     4、将文件名替换为需要统计的表格名（不带.xls），确认是否需要包括
@@ -24,7 +24,7 @@ import numpy as np
 
 #————————使用前请输入文件名，并确认是否包括上课时间—————————————
 #文件名                        
-filename = 'demo'
+filename = '20190225-0303'
 #是否包括上课时间，1为包括       
 course_enable = 0
 #——————————————————————————————————————————
@@ -133,16 +133,16 @@ for i in range (0,data1['人员编号'].unique().size):
     for j in range (1,8):
         num = i*7 + j - 1
         if (j!=6) and (j!=7):
-            if np.sum((mdata[num] != 0)*(mdata[num] >= 240)*(mdata[num] <= 520)) == 0:
+            if np.sum((mdata[num] != 0)*(mdata[num] >= 240)*(mdata[num] <= 520)) == 0: #4:00-8:40无记录
                 print (data2.iloc[i,1] + ' 星期 '+str(j)+' 上午迟到！')
-            if np.sum((mdata[num] != 0)*(mdata[num] >= 680)*(mdata[num] <= 850)) == 0:
+            if np.sum((mdata[num] != 0)*(mdata[num] >= 680)*(mdata[num] <= 850)) == 0: #11:20-14:10无记录
                 print (data2.iloc[i,1] + ' 星期 '+str(j)+' 上午早退&下午迟到！')
             if np.sum((mdata[num] != 0)*(mdata[num] >= 680)*(mdata[num] <= 850)) == 1:
-                if np.sum((mdata[num] != 0)*(mdata[num] >= 680)*(mdata[num] <= 780)) == 0:
+                if np.sum((mdata[num] != 0)*(mdata[num] >= 680)*(mdata[num] <= 780)) == 0: #11:20-13:00无记录
                     print (data2.iloc[i,1] + ' 星期 '+str(j)+' 上午早退！')
                 else:
                     print (data2.iloc[i,1] + ' 星期 '+str(j)+' 下午迟到！')
-            if np.sum((mdata[num] != 0)*(mdata[num] >= 1040)) == 0:
+            if np.sum((mdata[num] != 0)*(mdata[num] >= 1040)) == 0: #17:20后无记录
                 print (data2.iloc[i,1] + ' 星期 '+str(j)+' 下午早退！')
 
 
@@ -154,7 +154,7 @@ for i in range (0,data1['人员编号'].unique().size):
     morning = 0
     afternoon = 0
     evening = 0
-    last_sunday_evening = mdata[i*7][(mdata[i*7] != 0)*(mdata[i*7] <= 240)] #上周日晚上的记录
+    last_sunday_evening = mdata[i*7][(mdata[i*7] != 0)*(mdata[i*7] <= 240)] #上周日晚上的记录00:00-4:00
     last_sunday_eveningx = last_sunday_evening.shape[0]
     for j in range (1,8):
         num = i*7 + j - 1
@@ -162,63 +162,66 @@ for i in range (0,data1['人员编号'].unique().size):
         flag1 = 0
         flag2 = 0
         if j != 7:
-            ax = mdata[num][(mdata[num+1] != 0)*(mdata[num+1] <= 240)] #打卡超过零点的记录
+            ax = mdata[num][(mdata[num+1] != 0)*(mdata[num+1] <= 240)] #打卡超过零点的记录 00:00-4:00
             a = ax.shape[0]
-        bx = mdata[num][(mdata[num] != 0)*(mdata[num] > 240)*(mdata[num] <= 680)] #上午打卡的记录
+        bx = mdata[num][(mdata[num] != 0)*(mdata[num] > 240)*(mdata[num] <= 680)] #上午打卡的记录 4:00-11:20
         b = bx.shape[0]
-        cx = mdata[num][(mdata[num] != 0)*(mdata[num] > 850)*(mdata[num] <= 1040)] #下午打卡的记录
+        cx = mdata[num][(mdata[num] != 0)*(mdata[num] > 850)*(mdata[num] <= 1040)] #下午打卡的记录 14:10-17:20
         c = cx.shape[0]
-        dx = mdata[num][(mdata[num] != 0)*(mdata[num] > 1040)*(mdata[num] <= 1439)] #晚间打卡的记录
+        dx = mdata[num][(mdata[num] != 0)*(mdata[num] > 1040)*(mdata[num] <= 1439)] #晚间打卡的记录 17:20-23:59
         d = dx.shape[0]
-        ex = mdata[num][(mdata[num] != 0)*(mdata[num] >= 680)*(mdata[num] <= 850)] #中午打卡的记录
+        ex = mdata[num][(mdata[num] != 0)*(mdata[num] >= 680)*(mdata[num] <= 850)] #中午打卡的记录 11:20-14:10
         e = ex.shape[0]
         if b != 0:
-            if e == 0:
-                morning = morning +  np.max(bx) - np.min(bx) #早退
-            elif e >= 1:
+            #上午有打卡记录的情况
+            if e == 0: #上午早退的情况 
+                morning = morning +  np.max(bx) - np.min(bx)
+            elif e >= 1: #中午有记录那么就上午最早减去中午最早
                 morning = morning +  np.min(ex) - np.min(bx)
-                flag0 = 1
-                if e == 1:
-                    flag1 = 1
-        if c == 0:
-            if e == 1 and flag1 == 0 and d != 0:
+                flag0 = 1 #标记中午最早记录占用
+                if e == 1: 
+                    flag1 = 1 #标记中午唯一记录占用
+        if c == 0 : 
+            #下午无记录,但是中午有记录且未占用，并且晚上也有记录的情况
+            if e == 1 and flag1 == 0 and d != 0: 
                 afternoon = afternoon + np.min(dx) - np.max(ex)
-                flag2 = 1
-            elif e > 1 and flag0 == 1 and d != 0:
+            elif e > 1 and flag0 == 1 and d != 0: #如果中午被占用了一个，那么就从第二个（ex[1]）开始
                 afternoon = afternoon + np.min(dx) - ex[1]
-                flag2 = 1
             elif e > 1 and flag0 == 0 and d != 0:
                 afternoon = afternoon + np.min(dx) - ex[1]
-                flag2 = 1
-        else:
-            if e > 1 and d == 0 and flag1 == 0:
+            flag2 = 1 #标记晚上最早记录占用
+        else: 
+            #如果下午有记录的话
+            if e > 1 and d == 0 and flag1 == 0: #下午早退
                 afternoon = afternoon + np.max(cx) - np.max(ex)
-            elif e > 1 and d == 0 and flag1 == 1:
+            elif e > 1 and d == 0 and flag1 == 1: #下午早退且中午被占用一个
                 afternoon = afternoon + np.max(cx) - np.max(ex)
-            elif e > 1 and d != 0 and flag1 == 0:
+            elif e > 1 and d != 0 and flag1 == 0: #上午没打卡的情况
                 afternoon = afternoon + np.max(dx) - np.max(ex)
                 flag2 = 1
-            elif e > 1 and d != 0 and flag1 == 1:
+            elif e > 1 and d != 0 and flag1 == 1: #最正常的情况
                 afternoon = afternoon + np.max(dx) - np.max(ex)
                 flag2 = 1
-            elif e == 1 and d == 0 and flag1 == 0 :
+            elif e == 1 and d == 0 and flag1 == 0 : #上午没打卡的情况
                 afternoon = afternoon + np.max(cx) - np.max(ex)
-            elif e == 0 and d == 0:
+            elif e == 0 and d == 0: #下午迟到又早退的情况
                 afternoon = afternoon + np.max(cx) - np.min(cx)
-            elif e == 0 and d != 0:
+            elif e == 0 and d != 0: #中午没打卡的情况
                 afternoon = afternoon + np.max(dx) - np.min(cx)
                 flag2 = 1
             
-        if a != 0:
+        if a != 0: 
+            #打卡超过零点的情况
             evening2 = np.max(ax)
-        if d != 0:
-            if a != 0 and flag2 == 0:
+        if d != 0: 
+            #晚上有记录
+            if a != 0 and flag2 == 0: #过零点打卡的情况
                 evening = evening + 1439 - np.min(dx) + evening2
-            if a != 0 and flag2 == 1 and d != 1:
+            if a != 0 and flag2 == 1 and d != 1: #过零点打卡且晚上第一条记录被占用的情况
                 evening = evening + 1439 - dx[1] + evening2
-            if a == 0 and flag2 == 0:
+            if a == 0 and flag2 == 0: #未过零点打卡的情况
                 evening = evening + np.max(dx) - np.min(dx)
-            if a == 0 and flag2 == 1 and d != 1:
+            if a == 0 and flag2 == 1 and d != 1: #未过零点打卡的情况且晚上第一条记录被占用的情况
                 evening = evening + np.max(dx) - dx[1]
     if last_sunday_eveningx != 0:
         time.iloc[i,1] = round(((morning + afternoon + evening + np.max(last_sunday_evening))/60),3)
